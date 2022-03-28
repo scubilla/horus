65641234bfd94f50297d20b0c64b8e326974eae0
Simon Cubilla <bernardo.cubilla@mec.edu.py>
2022-03-28 15:03:45 -0400

    hasta static

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from app.core.erp.views import myFirstView, mySecondView
 
+app_name = 'erp'
+
 urlpatterns = [
-    path('uno/', myFirstView),
-    path('dos/', mySecondView)
+    path('uno/', myFirstView, name='vista1'),
+    path('dos/', mySecondView, name='vista2')
 ]
