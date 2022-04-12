a509f7abd40aff9c5a1acfe17e40376de028d21e
Simon Cubilla <bernardo.cubilla@mec.edu.py>
2022-04-12 15:08:23 -0400

    hasta lista bootstrap

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from app.core.erp.models import Category
 
+# lista basada en un afuncion
 def category_list(request):
     data = {
-
+        'title': 'Listado de Categorias',
+        'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
