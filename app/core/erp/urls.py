38fc95c58ac5a04b03ec3abcdd6782ac55f74adf
Simon Cubilla <bernardo.cubilla@mec.edu.py>
2022-04-18 15:00:11 -0400

    hasta vista_clase

--- app/core/erp/urls.py
+++ app/core/erp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from app.core.erp.views.category.views import category_list
+from app.core.erp.views.category.views import *
 
 #from app.core.erp.views import myFirstView, mySecondView
 
 app_name = 'erp'
 
 urlpatterns = [
-    path('category/list/', category_list, name='category_list'),
+    # path('category/list/', category_list, name='category_list'),
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
     #path('dos/', mySecondView, name='vista2')
 ]
--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from app.core.erp.models import Category
+from django.views.generic import ListView
+
+from app.core.erp.models import Category, Product
+
 
 # lista basada en un afuncion
 def category_list(request):
@@ -8,3 +11,17 @@ def category_list(request):
         'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    # cambiando el query set, sobrescribiendo comportamiento de metodos del get
+    def get_queryset(self):
+        return Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorias'
+        return context
