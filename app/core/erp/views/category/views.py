2b493fd7407173270724838296fe55b4ac9d08a4
Simon Cubilla <bernardo.cubilla@mec.edu.py>
2022-05-18 15:00:12 -0400

    hasta ajax

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
 from django.utils.decorators import method_decorator
+
 from app.core.erp.models import Category, Product
 
 
@@ -22,12 +25,19 @@ class CategoryListView(ListView):
     #def get_queryset(self):
     #    return Product.objects.all()
 
-    @method_decorator(login_required)
+    # @method_decorator(login_required)
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         #if request.method == 'GET':
         #   return redirect('erp:category_list2')
         return super().dispatch(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        # data = {'name': 'Simon'}
+        data = {}
+        cat = Category.objects.get(pk=request.POST['id'])
+        data['name'] = cat.name
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
