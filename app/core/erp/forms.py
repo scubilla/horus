da4fa971a7ef87ecc987922068c8f75887abd08e
Simon Cubilla <bernardo.cubilla@mec.edu.py>
2022-08-19 14:59:41 -0400

    hasta 27 12_30

--- app/core/erp/forms.py
+++ app/core/erp/forms.py
@@ -1,9 +1,34 @@
-from django.forms import ModelForm
+from django.forms import *
 from app.core.erp.models import Category
 
 # Create the form class.
 class CategoryForm(ModelForm):
+      def __init__(self, *args, **kwargs):
+          super().__init__(*args, **kwargs)
+          for form in self.visible_fields():
+              form.field.widget.attrs['class'] = 'form-control'
+              form.field.widget.attrs['autocomplete'] = 'off'
+          self.fields['name'].widget.attrs['autofocus'] = True
+
+
       class Meta:
         model = Category
         fields = '__all__'
+        labels = {
+            'name':'Nombre'
+        }
+        widgets = {
+            'name': TextInput(
+                attrs={
+                    'placeholder':'Ingrese un nombre',
+                }
+            ),
+            'desc': Textarea(
+                attrs={
+                    'placeholder': 'Ingrese descripción',
+                    'row': 3,
+                    'cols': 5
+                }
+            ),
+        }
 
